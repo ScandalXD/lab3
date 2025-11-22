81d74f56396a336643da9fdfa4d51dfb0c5c08fd
Artur Pylypenko <artur.pilipenko2018@gmail.com>
2025-11-22 09:30:18 +0100

    fix

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import {
   Alert,
   Keyboard,
   TouchableWithoutFeedback,
-  Image, // ← добавили Image
+  Image,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import TipOfTheDay from './TipOfTheDay';
